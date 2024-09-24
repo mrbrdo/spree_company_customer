21e159ddd470f9e6c66b7aaf721511d42fe4b97d
mrbrdo <mrbrdo@gmail.com>
2024-09-24 13:13:17 +0200

    fix for Zeitwerk

--- lib/spree_company_customer/engine.rb
+++ lib/spree_company_customer/engine.rb
@@ -13,11 +13,20 @@ module SpreeCompanyCustomer
       g.test_framework :rspec
     end
 
+    initializer "let the main autoloader ignore this engine's overrides" do
+      overrides = root.join("app/overrides")
+      Rails.autoloaders.main.ignore(overrides)
+    end
+
     def self.activate
       # enable Spree Core company field for Address
       ::Spree::Config.company = true
       ::Spree::PermittedAttributes.address_attributes.concat([:company_enabled, :company_vat_id, :company_vat_registered])
       ::Spree::Address.prepend SpreeCompanyCustomer::CoreExt::Spree::AddressWithCompanyInfo
+
+      Dir.glob(File.join(File.dirname(__FILE__), "../../app/overrides/*.rb")) do |c|
+        load(c)
+      end
     end
 
     config.to_prepare &method(:activate).to_proc
